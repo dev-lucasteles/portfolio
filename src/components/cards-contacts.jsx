6a54872ceb837f174872cb8144fbd305f28f8f4c
Lucas Teles <lucasteles2612@gmail.com>
2026-09-02 08:52:38 -0300

    Adjust card dimensions in cards-contacts component

--- src/components/cards-contacts.jsx
+++ src/components/cards-contacts.jsx
@@ -11,7 +11,7 @@ export default function CardsContatos( {contato, topico, img, alt, link}) {
             rel="noopener noreferrer"
             className="mx-auto content-center transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 animate-fade-in-right animate-duration-200 animate-fill-mode-both"
         >
-        <div className="mx-auto content-center bg-gray-800 w-16 h-16 rounded-[17vw] ">
+        <div className="mx-auto content-center bg-gray-800 w-15 h-15 rounded-[17vw] ">
             <img className="content-center mx-auto mt-2 w-10 h-10" src={img} alt={alt} />
         </div>
             <p className="content-center text-lg font-sans text-white text-xl font-bold text-center brightness-90 transition duration-300 hover:brightness-130">{topico}</p>
